e2b4226f1536c2a4d31c67d840a292b992a7de7f
1816013 <f zn fbkvsab k.htika@icloud.com>
2020-05-20 22:06:34 +0900

    追加

---
+++ Savingshooter/Assets/Scenes/script/MiniMapFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapFollow : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
