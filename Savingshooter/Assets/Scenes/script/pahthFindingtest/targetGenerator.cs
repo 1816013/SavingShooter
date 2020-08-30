dfabde648ebb1423ec2cdc5a75c1fb2a2e5d9467
1816013 <iahliugaeligfaliugefliw.com>
2020-08-31 06:59:29 +0900

    追加

---
+++ Savingshooter/Assets/Scenes/script/pahthFindingtest/targetGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class targetGenerator : MonoBehaviour
+{
+    public GameObject targetPrefab;
+    private GameObject target;
+
+    public void GenerateTarget(Vector3 pos)
+    {
+        target = GameObject.Instantiate(targetPrefab);
+        target.transform.position = new Vector3(pos.x, 1f, pos.z);
+    }
+ 
+}
